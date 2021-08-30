2fcec3033965adf9f9cdb2d16b776ee0ca7df823
gavan <guoming3@xiaomi.com>
2021-08-30 16:32:40 +0800

    cra samples.

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   routes: [
     { path: '/', component: '@/pages/index' },
     { path: '/table', component: '@/pages/table' },
+    { path: '/table2', component: '@/pages/table2' },
+    { path: '/form2', component: '@/pages/form2' },
+    { path: '/modalform', component: '@/pages/modalform' },
   ],
   fastRefresh: {},
   sula: {
