e7ee93c2aff55350849470d4228d5aaba9967c91
gavan <guoming3@xiaomi.com>
2021-08-30 16:47:53 +0800

    more samples.

--- .umirc.ts
+++ .umirc.ts
@@ -6,10 +6,15 @@ export default defineConfig({
   },
   routes: [
     { path: '/', component: '@/pages/index' },
+    { path: '/form2', component: '@/pages/form2' },
+    { path: '/form3', component: '@/pages/form3' },
     { path: '/table', component: '@/pages/table' },
     { path: '/table2', component: '@/pages/table2' },
-    { path: '/form2', component: '@/pages/form2' },
+    { path: '/table3', component: '@/pages/table3' },
+    { path: '/table4', component: '@/pages/table4' },
     { path: '/modalform', component: '@/pages/modalform' },
+    { path: '/dynamic', component: '@/pages/dynamic' },
+    { path: '/step', component: '@/pages/step' },
   ],
   fastRefresh: {},
   sula: {
